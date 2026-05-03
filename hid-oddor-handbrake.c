d63f2dc88e7a1c7d590eb0bdc2e8e9f8731e2c3c
fld <fld@users.noreply.github.com>
2026-05-04 00:08:52 +0300

    Replace ODDOR stuff with LTB stuff
    Changes USB vid & pid and occurances of oddor with ltbhb

--- hid-oddor-handbrake.c
+++ hid-oddor-handbrake.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- *  HID driver for ODDOR handbrake
+ *  HID driver for ltbhb handbrake
  *
  *  Copyright (c) 2025 Oleg Makarenko
  */
@@ -11,11 +11,11 @@
 #include <linux/module.h>
 #include <linux/usb.h>
 
-#define USB_VENDOR_ID_ODDOR               0x1021
-#define USB_PRODUCT_ID_ODDOR_HANDBRAKE    0x1888
+#define USB_VENDOR_ID_ltbhb               0x2023
+#define USB_PRODUCT_ID_ltbhb_HANDBRAKE    0x0803
 
 
-static int oddor_input_mapping(struct hid_device *hdev, struct hid_input *hi,
+static int ltbhb_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 		struct hid_field *field, struct hid_usage *usage,
 		unsigned long **bit, int *max)
 {
@@ -26,19 +26,19 @@ static int oddor_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 }
 
 
-static const struct hid_device_id oddor_devices[] = {
-	{ HID_USB_DEVICE(USB_VENDOR_ID_ODDOR, USB_PRODUCT_ID_ODDOR_HANDBRAKE) },
+static const struct hid_device_id ltbhb_devices[] = {
+	{ HID_USB_DEVICE(USB_VENDOR_ID_ltbhb, USB_PRODUCT_ID_ltbhb_HANDBRAKE) },
 	{ }
 };
 
-MODULE_DEVICE_TABLE(hid, oddor_devices);
+MODULE_DEVICE_TABLE(hid, ltbhb_devices);
 
-static struct hid_driver oddor_driver = {
-	.name = "hid-oddor",
-	.id_table = oddor_devices,
-    .input_mapping = oddor_input_mapping,
+static struct hid_driver ltbhb_driver = {
+	.name = "hid-ltbhb",
+	.id_table = ltbhb_devices,
+    .input_mapping = ltbhb_input_mapping,
 };
-module_hid_driver(oddor_driver);
+module_hid_driver(ltbhb_driver);
 
-MODULE_DESCRIPTION("HID driver for ODDOR handbrake");
+MODULE_DESCRIPTION("HID driver for LTB handbrake");
 MODULE_LICENSE("GPL");
